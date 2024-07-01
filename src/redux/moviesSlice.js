2636524459ff272e4a1138c0be9641e37ff0debd
ThemerGfx <thamer.gouider@gmail.com>
2024-07-01 12:44:34 +0200

    Commit to interview

--- src/redux/moviesSlice.js
+++ src/redux/moviesSlice.js
@@ -1,16 +1,16 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { movies$ } from '../movies';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { movies$ } from "../data/movies";
 
-export const fetchMovies = createAsyncThunk('movies/fetchMovies', async () => {
+export const fetchMovies = createAsyncThunk("movies/fetchMovies", async () => {
   const response = await movies$;
   return response;
 });
 
 const moviesSlice = createSlice({
-  name: 'movies',
+  name: "movies",
   initialState: {
     movies: [],
-    status: 'idle',
+    status: "idle",
     error: null,
     filters: [],
     currentPage: 1,
@@ -18,17 +18,19 @@ const moviesSlice = createSlice({
   },
   reducers: {
     deleteMovie: (state, action) => {
-      state.movies = state.movies.filter(movie => movie.id !== action.payload);
+      state.movies = state.movies.filter(
+        (movie) => movie.id !== action.payload
+      );
     },
     toggleLike: (state, action) => {
-      const movie = state.movies.find(movie => movie.id === action.payload);
+      const movie = state.movies.find((movie) => movie.id === action.payload);
       if (movie) {
         movie.likes += 1;
         movie.dislikes -= 1;
       }
     },
     toggleDislike: (state, action) => {
-      const movie = state.movies.find(movie => movie.id === action.payload);
+      const movie = state.movies.find((movie) => movie.id === action.payload);
       if (movie) {
         movie.likes -= 1;
         movie.dislikes += 1;
@@ -47,19 +49,26 @@ const moviesSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(fetchMovies.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
       .addCase(fetchMovies.fulfilled, (state, action) => {
-        state.status = 'succeeded';
+        state.status = "succeeded";
         state.movies = action.payload;
       })
       .addCase(fetchMovies.rejected, (state, action) => {
-        state.status = 'failed';
+        state.status = "failed";
         state.error = action.error.message;
       });
   },
 });
 
-export const { deleteMovie, toggleLike, toggleDislike, setFilters, setPage, setItemsPerPage } = moviesSlice.actions;
+export const {
+  deleteMovie,
+  toggleLike,
+  toggleDislike,
+  setFilters,
+  setPage,
+  setItemsPerPage,
+} = moviesSlice.actions;
 
 export default moviesSlice.reducer;
